5a9969c32a5aa4395581a22453938a19e6a64e21
tavozalixi <143713772+tavozalixi@users.noreply.github.com>
2024-08-23 12:05:45 -0300

    Add files via upload

---
+++ java.js
@@ -0,0 +1,23 @@
+const caixaPrincipal = document.querySelector("caixaPrincipal");
+const caixaPerguntas = document.querySelector("caixaPerguntas");
+const caixaAlternativas = document.querySelector("caixaAlternativas");
+const caixaResultado = document.querySelector("caixaResultado");
+const textoResultado = document.querySelector("texto-Resultado");
+
+const perguntas = [
+    {
+        enunciado: "Pergunta 1",
+        alternativas: [
+            "Alternativa 1",
+            "Alternativa 2"
+        ]
+    },
+    {
+        enunciado: "Pergunta 2",
+        alternativas: [
+            "Alternativa 1",
+            "Alternativa 2"
+        ]
+    }
+];
+
